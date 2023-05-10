51a0a85b82de2d70af99da7f0b9d3497794a79c9
Biloliddin99 <newprogramm728@gmail.com>
2023-05-10 19:49:54 +0500

    binding added

--- app/src/main/java/com/example/tasbeh/MainActivity.kt
+++ app/src/main/java/com/example/tasbeh/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.tasbeh
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tasbeh.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var sharedPreferences: SharedPreferences
+    private var number = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        sharedPreferences.edit().putString("key", "value").apply()
+        number = sharedPreferences.getInt("number", 0)
+        binding.tv
+
+        binding.btn.setOnClickListener {
+            number++
+            var editor = sharedPreferences.edit()
+            editor.putInt("number",number)
+            editor.apply()
+            binding.tv.text = number.toString()
+
+        }
     }
 }
